a8fd8ee42ca8091adf3550d428385e57fe22e5c6
lenk <lenk10056@cgmh.org.tw>
2021-12-03 14:22:45 +0800

    Add project files.

---
+++ LCode/ListNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace LCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+        public static ListNode Create(IList<int> vs)
+        {
+            if (vs == null || vs.Count()==0)
+            { return null; }
+            ListNode head = new ListNode(vs[0]);
+            ListNode curr = head;
+            for (int i = 1; i < vs.Count; i++)
+            {
+                ListNode n = new ListNode(vs[i]);
+                curr.next = n;
+                curr = curr.next;
+            }
+            return head;
+        }
+        public List<int> ToList()
+        {
+            List<int> res = new List<int>();
+            ListNode curr = this;
+            while (curr!=null)
+            {
+                res.Add(curr.val);
+                curr = curr.next;
+            }
+            return res;
+        }
+    }
+}
---
+++ LCodeTEST/ListNodeTEST.cs
@@ -0,0 +1,36 @@
+﻿using LCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+namespace LCodeTEST
+{
+    [TestClass]
+    public class ListNodeTEST
+    {
+        [TestMethod]
+        public void ToList()
+        {
+            ListNode ln = new ListNode(1);
+            ln.ToList().Should().Equal(new List<int> { 1});
+            ln.next = new ListNode(2);
+            ln.ToList().Should().Equal(new List<int> { 1,2 });
+            ln.next.next = new ListNode(3);
+            ln.ToList().Should().Equal(new List<int> { 1, 2,3 });
+        }
+        [TestMethod]
+        public void Create()
+        {
+            ListNode ln = ListNode.Create(null);
+            ln.Should().BeNull();
+            ln = ListNode.Create(new List<int>());
+            ln.Should().BeNull();
+            ln = ListNode.Create(new List<int> { 4});
+            ln.ToList().Should().Equal(new List<int> { 4});
+            ln = ListNode.Create(new List<int> {4,5,6 });
+            ln.ToList().Should().Equal(new List<int> {4,5,6 });
+
+        }
+    }
+}
