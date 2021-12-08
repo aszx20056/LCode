23090b1a5305341bb479be1af8ca56a5ebf0358e
lenk <lenk10056@cgmh.org.tw>
2021-12-08 11:28:45 +0800

    Add LcArr & TwoSum

---
+++ LCode/LcArr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCode
+{
+    public class LcArr
+    {
+        /// <summary>
+        /// 1. Two Sum
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int> {
+                {nums[0],0 }
+            };
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dict.ContainsKey(diff))
+                {
+                    return new int[] { dict[diff], i };
+                }
+                dict.TryAdd(nums[i],i);
+            }
+            return null;
+        }
+    }
+}
---
+++ LCodeTEST/LcArrTEST.cs
@@ -0,0 +1,22 @@
+﻿using LCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+namespace LCodeTEST
+{
+    [TestClass]
+    public class LcArrTEST
+    {
+        LcArr lc = new LcArr();
+        [TestMethod]
+        public void TwoSum()
+        {
+            lc.TwoSum(new int[] { 2, 7, 11, 15 }, 9).Should().Equal(new int[] {0,1 });
+            lc.TwoSum(new int[] { 3, 2, 4 }, 6).Should().Equal(new int[] { 1, 2 });
+            lc.TwoSum(new int[] { 3, 3 }, 6).Should().Equal(new int[] { 0, 1 });
+            lc.TwoSum(new int[] { 3, 3 }, 20).Should().BeNull();
+        }
+    }
+}
