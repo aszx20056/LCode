efde408618f540a3071a5e34fc0115d74cfde95f
lenk <lenk10056@cgmh.org.tw>
2021-12-10 18:06:40 +0800

    add LcTree & InorderTraversal

---
+++ LCode/LcTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCode
+{
+    public class LcTree
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+     
+            List<int> res = new List<int>();
+            Stack<TreeNode> st = new Stack<TreeNode>();
+            TreeNode curr = root;
+            while (curr!=null || st.Count>0)
+            {
+                while (curr!=null)
+                {
+                    st.Push(curr);
+                    curr = curr.left;
+                }
+                curr = st.Pop();
+                res.Add(curr.val);
+                curr = curr.right;
+            }
+            return res;
+        }
+        private void InorderTraversal_SUB(TreeNode root,List<int> res)
+        {
+            if (root == null)
+            { return; }
+            InorderTraversal_SUB(root.left, res);
+            res.Add(root.val);
+            InorderTraversal_SUB(root.right, res);
+        }
+    }
+}
---
+++ LCodeTEST/LcTreeTEST.cs
@@ -0,0 +1,24 @@
+﻿using LCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+namespace LCodeTEST
+{
+    [TestClass]
+    public class LcTreeTEST
+    {
+        LcTree lc = new LcTree();
+        [TestMethod]
+        public void InorderTraversal()
+        {
+            lc.InorderTraversal(TreeNode.Create(new int?[] { 1, null, 2, 3 })).Should().Equal(new List<int> { 1, 3, 2 });
+            lc.InorderTraversal(TreeNode.Create(null)).Should().Equal(new List<int>());
+            lc.InorderTraversal(TreeNode.Create(new int?[] { 1 })).Should().Equal(new List<int> { 1 });
+            lc.InorderTraversal(TreeNode.Create(new int?[] { 1, 2 })).Should().Equal(new List<int> { 2,1 });
+            lc.InorderTraversal(TreeNode.Create(new int?[] { 1, null, 2 })).Should().Equal(new List<int> { 1, 2 });
+
+        }
+    }
+}
